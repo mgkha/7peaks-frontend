7c61ab47e9e3b920b92902d11ca7fb60b2598464
Kaung Htet Aung <kaung.h.ag@gmail.com>
2022-12-07 01:56:42 +0630

    fix vanilla extract webpack config

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,29 @@
 const path = require("path");
 const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin");
+const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
 
 module.exports = {
+  style: {
+    css: {
+      loaderOptions: {
+        url: false,
+      },
+    },
+  },
   webpack: {
     plugins: [new VanillaExtractPlugin()],
     alias: {
       "@": path.resolve(__dirname, "src"),
+    },
+    configure: (webpackConfig) => {
+      const moduleScopePlugin = webpackConfig.resolve.plugins.find(
+        (plugin) => plugin instanceof ModuleScopePlugin
+      );
+      moduleScopePlugin.allowedPaths.push(
+        path.resolve(__dirname, "node_modules/@vanilla-extract/webpack-plugin")
+      );
+
+      return webpackConfig;
     },
   },
 };
